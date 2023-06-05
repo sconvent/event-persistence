cd2a4f51d06fdb1fdf0e32cc02d4027aa881673b
Simon Convent <simon@convnet.de>
2023-06-05 18:24:38 +0200

    Improve MQTT event handling

--- Mqtt.ts
+++ Mqtt.ts
@@ -6,29 +6,42 @@ const mqttHost = process.env.MQTT_HOST || "localhost"
 const mqttPort = process.env.MQTT_PORT || 1883
 const mqttUser = process.env.MQTT_USER || undefined
 const mqttPassword = process.env.MQTT_PASSWORD
-const mqttTopics: string = process.env.MQTT_TOPICS || ""
+const mqttTopics: string | undefined = process.env.MQTT_TOPICS || undefined
 const mqttClientId = process.env.MQTT_CLIENT_ID
 
 export function setupMqtt(handleEvent: any) {
-  if(mqttEnabled) {
-      console.log("Starting MQTT client")
-      let client = mqtt.connect(`mqtt://${mqttHost}:${mqttPort}`, {username: mqttUser, password: mqttPassword, clientId: mqttClientId})
-      // callbacks
-      client.on("connect", () => {
-          console.log("Connected")
-          mqttTopics.split(",").forEach((topic) => {
-              client.subscribe(topic)
-          })
-      })
-  
-      client.on("error", (error) => {
-          console.log(`Error: ${error}`)
-      })
-  
-      client.on("message", (topic, message) => {
-          console.log(`Received message on topic ${topic}: ${message.toString()}`)
-          handleEvent({topic: topic}, JSON.parse(message.toString()))
-          //handleEvent({topic: topic}, JSON.parse(message.toString()))
-      })
-  }
+    if (mqttEnabled) {
+        console.log("Starting MQTT client")
+        let client = mqtt.connect(`mqtt://${mqttHost}:${mqttPort}`, { username: mqttUser, password: mqttPassword, clientId: mqttClientId })
+
+        // callbacks
+        client.on("connect", () => {
+            console.log("MQTT connection established")
+            if (mqttTopics) {
+                mqttTopics.split(",").forEach((topic) => {
+                    client.subscribe(topic)
+                })
+            }
+            else {
+                console.log("No MQTT topics defined")
+            }
+        })
+
+        client.on("reconnect", () => {
+            console.log("MQTT connection reconnect started")
+        })
+
+        client.on("error", (error) => {
+            console.log(`MQTT connection error: ${error}`)
+        })
+
+        client.on("close", () => {
+            console.log("Closed connection")
+        })
+
+        client.on("message", (topic, message) => {
+            console.log(`Received message on topic ${topic}: ${message.toString()}`)
+            handleEvent({ topic: topic }, JSON.parse(message.toString()))
+        })
+    }
 }
